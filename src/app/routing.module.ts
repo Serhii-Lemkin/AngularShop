9d37004b030fa357393b799234a0190acc87bb3d
Serhii Lemkin <JackFlit51997@gmail.com>
2022-11-20 01:51:51 +0200

    starter init bootstrap

---
+++ src/app/routing.module.ts
@@ -0,0 +1,8 @@
+import { Routes,RouterModule } from "@angular/router"
+
+const appRoutes: Routes =[
+    //{path: '', component: },
+    
+]
+
+export const routing = RouterModule.forRoot(appRoutes)
